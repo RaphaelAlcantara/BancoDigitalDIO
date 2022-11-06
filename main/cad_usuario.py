7701e1288c9623a0070ed8ded4ef3c613f966f10
RaphaelAlcantara <rvfra@discente.ifpe.edu.br>
2022-11-06 00:34:36 -0300

    cad user

---
+++ main/cad_usuario.py
@@ -0,0 +1,57 @@
+import validador_cpf
+
+
+# cadastra usuario
+
+
+def cadastrar_usuario(usuario_cadastrado, cpf_cadastrado):
+    usuario = []
+
+    while True:
+        cpf = input('Digite seu CPF: ')
+        cpf = validador_cpf.valida_cpf(cpf)
+        if not cpf:
+            if cpf in cpf_cadastrado:
+                print('CPF já cadastrado')
+            print('CPF invalido')
+            continue
+        else:
+            cpf_cadastrado.append(cpf)
+            usuario.append(cpf)
+            print('CPF cadastrado com sucesso')
+            break
+
+    nome = input('Digite o nome: ')
+    data_nascimento = data()
+    endereco = input('Digite o endereço: '), \
+               input('Digite o número: '), \
+               input('Digite o Bairro: '), \
+               input('Digite a cidade: '), \
+               input('Digite o estado: '), \
+ \
+    usuario.append(nome)
+    usuario.append(data_nascimento)
+    usuario.append(endereco)
+    usuario_cadastrado.append(usuario)
+
+    print('Usuário cadastrado com sucesso')
+    return usuario_cadastrado, cpf_cadastrado
+
+
+# lista usuario
+def listar_usuario(usuario_cadastrado):
+    for usuario in usuario_cadastrado:
+        print(usuario)
+
+
+def data():
+    print('Digite a data de nascimento')
+    day = int(input('Insira o dia: '))
+    month = int(input('Insira o mês: '))
+    year = int(input('Insira o Ano: '))
+
+    data_nsc = "{}/{}/{}".format(day, month, year)
+    return data_nsc
+
+
+
--- main/main.py
+++ main/main.py
@@ -1,12 +1,23 @@
 import funcoes
+import cad_usuario
+
+# import validador_cpf
 
 menu = """
-[d] Depositar
-[s] Sacar
-[e] Extrato
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[4] Cadastrar
+[5] Criar Conta
 [q] Sair 
 =>"""
 
+
+# variaveis globais
+cpf_cadastrado = []
+usuario_cadastrado = []
+contas = []
+n_conta = 1
 saldo = 0
 limite = 500
 extrato = []
@@ -17,13 +28,13 @@ while True:
     opcao = input(menu)
     if opcao == 'q':
         break
-    elif opcao == 'd':
+    elif opcao == '1':
         print('Depositar')
         print("-" * 30)
         valor = float(input('Valor do deposito: '))
         saldo, extrato = funcoes.deposito(saldo, valor, extrato)
 
-    elif opcao == 's':
+    elif opcao == '2':
         print('Sacar')
         print("-" * 30)
 
@@ -31,7 +42,21 @@ while True:
         saldo, extrato, numero_saque = funcoes.sacar(saldo=saldo, valor=valor, extrato=extrato, limite=limite,
                                                      numero_saque=numero_saque, LIMITE_SAQUE=LIMITE_SAQUE)
 
-    elif opcao == 'e':
+    elif opcao == '3':
         print('Extrato')
         print("-" * 30)
         saldo, extrato = funcoes.tirar_extrato(saldo, extrato=extrato)
+
+    elif opcao == '4':
+        print('Cadastrar')
+        print("-" * 30)
+
+        opcao = input("[1] Cadastrar\n[2] Listar\n=>")
+
+        if opcao == '1':
+            usuario_cadastrado, cpf_cadastrado = cad_usuario.cadastrar_usuario(usuario_cadastrado, cpf_cadastrado)
+
+        elif opcao == '2':
+            cad_usuario.listar_usuario(usuario_cadastrado)
+
+
